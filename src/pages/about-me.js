310d531c39d752f94179346a3ef4d325c539cb03
Scott Smith <scott@scottosmith.net>
2020-08-04 22:37:40 -0700

    Update about me content

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
+import styled from 'styled-components';
 
 import Card from '../components/ui/card';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const AboutMe = () => (
-  <Layout>
-    <SEO title='About Me' />
-    <Card>
-      <h2>A little about me:</h2>
-      <img></img>
-      <p>I take pictures, I travel, I code, I make music. My dog Rhonda is the coolest thing on this here planet Earth! I love my motorcycle, can't get enough of my van, and am working on getting my private pilot license. I'm endlessly scouting the world around me for all the nooks and crannies that the rest of the world hasn't noticed yet.</p>
-    </Card>
-  </Layout>
-);
+const AboutImg = styled(Img)`
+  margin: 1em auto;
+  border: 1px solid #212121;
+`;
+
+const AboutMe = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "me2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 700, quality: 100) {
+            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title='About Me' />
+      <Card>
+        <h2>A little about me:</h2>
+        <AboutImg title='On a boat' fadeIn fluid={data.placeholderImage.childImageSharp.fluid} />
+        <p>
+          I take pictures, I travel, I code, I make music. My dog Rhonda is the coolest thing on this here planet Earth! 
+          I love my motorcycle, can't get enough of my van, and am working on getting my private pilot license. 
+          I'm endlessly scouting the world around me for all the nooks and crannies that the rest of the world hasn't noticed yet.
+        </p>
+        <p>
+          More to come!
+        </p>
+      </Card>
+    </Layout>
+  );
+};
 
 export default AboutMe;
