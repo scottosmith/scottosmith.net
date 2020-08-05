f660dd49e13dbd372f840c2b5877fa6924f10d5e
Scott Smith <scott@scottosmith.net>
2020-08-04 22:57:33 -0700

    Update site content (#3)
    * Update site colors, index page content
    
    * Updating index and about me content
    
    * Update about me content
    
    * More on index, font size increase
    
    * Font adjustment
    
    * About me update

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
+import styled from 'styled-components';
 
 import Card from '../components/ui/card';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const AboutMe = () => (
-  <Layout>
-    <SEO title='About Me' />
-    <Card>
-      <h1>A little about me:</h1>
-      <p>I'm tall.</p>
-    </Card>
-  </Layout>
-);
+const AboutImg = styled(Img)`
+  margin: 1em auto;
+  border: 1.5px solid #212121;
+`;
+
+const AboutMe = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "me2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 100) {
+            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title='About Me' />
+      <Card>
+        <h2>A little about me:</h2>
+        <AboutImg title='On a boat' alt='On a boat' fluid={data.placeholderImage.childImageSharp.fluid} />
+        <p>
+          I take pictures, I travel, I code, I make music. My dog Rhonda is the coolest thing on this here planet Earth! 
+          I love my motorcycle, can't get enough of my van, and am working on getting my private pilot license. 
+          I'm endlessly scouting the world around me for all the nooks and crannies that the rest of the world hasn't noticed yet.
+        </p>
+        <p>
+          More to come!
+        </p>
+      </Card>
+    </Layout>
+  );
+};
 
 export default AboutMe;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Card from '../components/ui/card';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+
+const Currents = styled.div`
+  margin-left: .5em;
+`;
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title='Home' />
       <Card>
-        Hello! I'm Scott Smith. I'm a full stack developer with a particular interest in all things front end. I love JavaScript, React, and all things CSS. 
+        <p>
+          Hello! I'm Scott Smith and this is my portfolio. I'm a full stack developer, but with a particular love of all things front end. Having a direct influence on the user experience and being able to build the interfaces that connect people to services are what keep me going!
+        </p>
+        <Currents>
+          <h3>Current obsessions:</h3>
+          <ul>
+            <li><a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a></li>
+            <li><a href="gatsbyjs.org/" target="_blank" rel="noreferrer">Gatsby</a></li>
+            <li><a href="https://graphql.org/" target="_blank" rel="noreferrer">GraphQL</a></li>
+            <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations" target="_blank" rel="noreferrer">CSS Animations</a></li>
+            <li><a href="https://styled-components.com/" target="_blank" rel="noreferrer">Styled Components</a></li>
+          </ul>
+        </Currents>
+        <p>
+          If you can see this, the site is still being worked on! This <a href="https://jamstack.org/" target="_blank" rel="noreferrer">Jamstack</a> site is built by me using Gatsby and Styled Components, and is continuously deployed by <a href="https://buddy.works/" target="_blank" rel="noreferrer">Buddy</a>.
+        </p>
       </Card>
     </Layout>
   );
