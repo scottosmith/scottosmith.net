4f61aa2c30f58e9dbfde7b71142ef0110658727b
Scott Smith <scott@scottosmith.net>
2020-08-06 15:17:23 -0700

    Convert to <Project> component

---
+++ src/components/ui/project.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const ProjectText = styled.p`
+  font-size: .9em;
+
+  @media (min-width: 768px) {
+    font-size: 1em;
+  }
+`;
+
+const ProjectImg = styled.img`
+  display: block;
+  max-width: 80%;
+  margin: 1em auto;
+  border: 1.5px solid var(--img-border);
+`;
+
+const ProjectLinks = styled.div`
+  font-size: .9em;
+  margin: .75em 0;
+  display: flex;
+  justify-content: space-between;
+  width: 7em;
+
+  @media (min-width: 768px) {
+    font-size: 1em;
+    margin: 1em 0;
+  }
+`;
+
+const Project = props => {
+  return (
+    <>
+      <h3>{props.title}</h3>
+      <ProjectImg src={props.imgUrl} />
+      <ProjectLinks>
+        <a href={props.websiteUrl} target="_blank" rel="noreferrer">Website</a> |
+        <a href={props.githubUrl} target="_blank" rel="noreferrer">Github</a>
+      </ProjectLinks>
+      <ProjectText>
+        {props.children}
+      </ProjectText>
+    </>
+  );
+};
+
+Project.propTypes = {
+  show: PropTypes.bool,
+  closed: PropTypes.func
+}
+
+Project.defaultProps = {
+  show: 'false'
+}
+
+export default Project;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import styled from 'styled-components'
 
 import CardContainer from '../components/ui/card-container';
 import Card from '../components/ui/card';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-
-const ProjectText = styled.p`
-  font-size: .9em;
-
-  @media (min-width: 768px) {
-    font-size: 1em;
-  }
-`;
-
-const ProjectImg = styled.img`
-  display: block;
-  max-width: 80%;
-  margin: 1em auto;
-  border: 1.5px solid var(--img-border);
-`;
-
-const ProjectLinks = styled.div`
-  font-size: .9em;
-  margin: .75em 0;
-  display: flex;
-  justify-content: space-between;
-  width: 7em;
-
-  @media (min-width: 768px) {
-    font-size: 1em;
-    margin: 1em 0;
-  }
-`;
+import Project from '../components/ui/project';
 
 const Projects = () => {
   return (
@@ -40,18 +12,16 @@ const Projects = () => {
       <SEO title='Projects' />
       <CardContainer>
         <Card>
-          <h3>The Weather</h3>
-          <ProjectImg src="https://i.imgur.com/2M7oJwY.png" />
-          <ProjectLinks>
-            <a href="https://weather.scottosmith.net/" target="_blank" rel="noreferrer">Website</a> |
-            <a href="https://github.com/scottosmith/the-weather" target="_blank" rel="noreferrer">Github</a>
-          </ProjectLinks>
-          <ProjectText>
+          <Project 
+          title="The Weather" 
+          imgUrl="https://i.imgur.com/2M7oJwY.png" 
+          websiteUrl="https://weather.scottosmith.net/" 
+          githubUrl="https://github.com/scottosmith/the-weather" >
             Using React, CSS modules, and the OpenWeatherMap API, I made a basic weather app. 
             This was fun because I've always wanted to make a custom weather app. I have plans to 
             adapt this to my flying and hiking needs, to show the specific information I'm always 
             looking for without all the excess.
-          </ProjectText>
+          </Project>
         </Card>
         <Card>
           <h3>Project 2</h3>
