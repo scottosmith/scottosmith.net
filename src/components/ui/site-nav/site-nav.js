2aa54ef79a2873f3bf74c939df8d8223680fc04b
Scott Smith <scott@scottosmith.net>
2020-08-06 22:30:04 -0700

    Add Resume link to <SiteNav>

--- src/components/ui/site-nav/site-nav.js
+++ src/components/ui/site-nav/site-nav.js
@@ -8,6 +8,7 @@ const SiteNav = props => (
   <SiteNavContainer>
     <SiteNavItem fade duration={.4} to="/about-me" show={props.show.toString()}>About Me</SiteNavItem>
     <SiteNavItem fade duration={.4} to="/projects" show={props.show.toString()}>Projects</SiteNavItem>
+    <SiteNavItem href="https://docs.google.com/document/d/1Fh2PoAmJKpbjjDKkW6PcoWVB-9NYbhKFDmPDztnm8LE/edit?usp=sharing" target="_blank" rel="noreferrer" show={props.show.toString()}>Resume</SiteNavItem>
   </SiteNavContainer>
 );
 
