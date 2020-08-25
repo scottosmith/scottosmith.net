b510cedfcf4eb613f3385dca6f9fd28efe3ab27a
Scott Smith <scott@scottosmith.net>
2020-08-25 18:20:20 -0500

    Fix link, change <NerdList> to <PaddedList>

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Card from '../components/ui/cards/card';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NerdList = styled.div`
+const PaddedList = styled.div`
   margin-left: .5em;
 `;
 
@@ -19,7 +19,7 @@ const IndexPage = () => {
           Having a direct influence on the user experience and being able to build the interfaces that connect people to services are what 
           drive me to keep learning, experimenting, and growing. Over 15 years in the development world and there's still so much to learn!
         </p>
-        <NerdList>
+        <PaddedList>
           <h3>Things currently occupying the techy part of my brain:</h3>
           <ul>
             <li><a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a></li>
@@ -28,12 +28,18 @@ const IndexPage = () => {
             <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations" target="_blank" rel="noreferrer">CSS Animations</a></li>
             <li><a href="https://styled-components.com/" target="_blank" rel="noreferrer">Styled Components</a></li>
           </ul>
-        </NerdList>
+        </PaddedList>
         <p>
           As you can see, I don't have a blog. I know, right? Very not 2020... but as much as I love to write and share knowledge, it isn't my forte, 
           and that's ok! Everything is not for everyone. There are some amazing people out there in the world who are better skilled at teaching methods 
           and styles that really get the point across.
         </p>
+        <PaddedList>
+          <h3>Here are a few teach teachers that inspire me:</h3>
+          <ul>
+            <li><a href="https://mobile.twitter.com/dan_abramov">Dan Abramov</a></li>
+          </ul>
+        </PaddedList>
       </Card>
     </Layout>
   );
