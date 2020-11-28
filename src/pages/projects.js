4b577f17df40f4174656084362d65f34202b0d98
Scott Smith <scott@scottosmith.net>
2020-11-27 20:51:37 -0600

    Add cList, remove photo blog, add header

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,52 +10,68 @@ import ExLink from '../components/ui/ex-link';
 const Projects = () => {
   return (
     <Layout>
-      <SEO title='Projects' />
+      <SEO title="Projects" />
       <CardContainer>
+        <p>
+          All projects built, deployed, and hosted on{' '}
+          <ExLink url="https://www.netlify.com/">Netlify</ExLink>.
+        </p>
         <Card>
-          <Project 
-          title="The Weather" 
-          imgUrl="https://i.imgur.com/2M7oJwY.png" 
-          websiteUrl="https://weather.scottosmith.net/" 
-          githubUrl="https://github.com/scottosmith/the-weather">
-            Using <ExLink url="https://reactjs.org">React</ExLink>,&nbsp; 
-            <ExLink url="https://github.com/css-modules/css-modules">CSS modules</ExLink>, 
-            and the <ExLink url="https://openweathermap.org/api">OpenWeatherMap API</ExLink>, 
-            I made a basic weather app. This was fun because I've always wanted to make a custom weather app. I have 
-            plans to adapt this to my flying and hiking needs, to show the specific information I'm always looking 
-            for without having to weed through all the excess. I use <ExLink url="https://www.netlify.com/">Netlify</ExLink> to build/deploy/host this project.
+          <Project
+            title="cList"
+            imgUrl="https://i.imgur.com/be83l5y.png"
+            websiteUrl="https://clist.scottosmith.net/"
+            githubUrl="https://github.com/scottosmith/clist"
+          >
+            This is a project I decided to do to scratch a personal itch. I
+            wanted an easy way to search through Craigslist and add items to
+            different lists, sort of like a shopping list. I tend to compare ads
+            when looking for certain things and this makes it easier to keep
+            track of the different ones I'm interested in.{' '}
+            <ExLink url="https://reactjs.org">React</ExLink> and{' '}
+            <ExLink url="https://styled-components.com/">
+              Styled Components
+            </ExLink>{' '}
+            for this one. This has been a fun project to work on!
           </Project>
         </Card>
         <Card>
-          <Project 
-          title="Photo Blog Front End" 
-          imgUrl="https://i.imgur.com/Imtllb5.png"
-          githubUrl="https://github.com/scottosmith/sosweb-frontend">
-            I have been slowly working on a blog to showcase my photography, building each piece of the stack myself: front-end, API, 
-            database, and server. The front-end is built with React, starting with an out-of-the-box <ExLink url="https://github.com/facebook/create-react-app">create-react-app</ExLink> instance 
-            and using <ExLink url="https://sass-lang.com/">Sass</ExLink>, one my favorite CSS 
-            tools ever. Sass makes stylesheets much easier to build and maintain. The syntax just makes sense. Not publicly available 
-            quite yet, but soon. I've got photos I want to share that are stacking up!
+          <Project
+            title="The Weather"
+            imgUrl="https://i.imgur.com/2M7oJwY.png"
+            websiteUrl="https://weather.scottosmith.net/"
+            githubUrl="https://github.com/scottosmith/the-weather"
+          >
+            Using <ExLink url="https://reactjs.org">React</ExLink>,&nbsp;
+            <ExLink url="https://github.com/css-modules/css-modules">
+              CSS modules
+            </ExLink>
+            , and the{' '}
+            <ExLink url="https://openweathermap.org/api">
+              OpenWeatherMap API
+            </ExLink>
+            , I made a basic weather app. This was fun because I've always
+            wanted to make a custom weather app. I have plans to adapt this to
+            my flying and hiking needs, to show the specific information I'm
+            always looking for without having to weed through all the excess.
           </Project>
         </Card>
         <Card>
-          <Project 
-          title="Photo Blog Back End" 
-          githubUrl="https://github.com/scottosmith/sosweb-api">
-            To learn some new back end skills and keep the "full stack" part of my title current, I built a <ExLink url="https://graphql.org/">GraphQL</ExLink> API 
-            for my blog, powered by a <ExLink url="https://www.mongodb.com/">MongoDB</ExLink> database. The more I read about GraphQL and NoSQL DBs, the more I 
-            wanted to build with them so I could understand them better and experience the differences from other tech I've worked with like REST APIs and SQL DBs.
-          </Project>
-        </Card>
-        <Card>
-          <Project 
-          title="scottosmith.net" 
-          websiteUrl="https://scottosmith.net/" 
-          githubUrl="https://github.com/scottosmith/scottosmith.net">
-            Using the <ExLink url="https://github.com/gatsbyjs/gatsby-starter-default">Gatsby default starter</ExLink>, I built my portfolio/personal website 
-            using <ExLink url="https://styled-components.com/">Styled Components</ExLink>, something I am THOROUGHLY enjoying using. 
-            This <ExLink url="https://jamstack.org/">Jamstack</ExLink> site is continuously deployed and hosted by&nbsp;
-            <ExLink url="https://www.netlify.com/">Netlify</ExLink>. 
+          <Project
+            title="scottosmith.net"
+            websiteUrl="https://scottosmith.net/"
+            githubUrl="https://github.com/scottosmith/scottosmith.net"
+          >
+            Using the{' '}
+            <ExLink url="https://github.com/gatsbyjs/gatsby-starter-default">
+              Gatsby default starter
+            </ExLink>
+            , I built my portfolio/personal website using{' '}
+            <ExLink url="https://styled-components.com/">
+              Styled Components
+            </ExLink>
+            , something I am THOROUGHLY enjoying using.
+            <ExLink url="https://jamstack.org/">Jamstack</ExLink> is great!
           </Project>
         </Card>
       </CardContainer>
