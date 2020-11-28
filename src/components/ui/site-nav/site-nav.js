e481823b81986f5efb6624d42a99728f16664c30
Scott Smith <scott@scottosmith.net>
2020-11-27 21:02:53 -0600

    Add cList Project (#34)
    * Add cList, remove photo blog, add header
    
    * Add blog link, smaller mobile menu width
    
    * Add blog link to about me text

--- src/components/ui/site-nav/site-nav.js
+++ src/components/ui/site-nav/site-nav.js
@@ -6,18 +6,51 @@ import SiteNavItem from './site-nav-item';
 
 const SiteNav = props => (
   <SiteNavContainer>
-    <SiteNavItem fade duration={.4} to="/about-me" show={props.show.toString()} onClick={props.itemClicked}>About Me</SiteNavItem>
-    <SiteNavItem fade duration={.4} to="/projects" show={props.show.toString()} onClick={props.itemClicked}>Projects</SiteNavItem>
-    <SiteNavItem href="https://docs.google.com/document/d/1Fh2PoAmJKpbjjDKkW6PcoWVB-9NYbhKFDmPDztnm8LE/edit?usp=sharing" target="_blank" rel="noreferrer" show={props.show.toString()} onClick={props.itemClicked}>Resume</SiteNavItem>
+    <SiteNavItem
+      fade
+      duration={0.4}
+      to="/about-me"
+      show={props.show.toString()}
+      onClick={props.itemClicked}
+    >
+      About Me
+    </SiteNavItem>
+    <SiteNavItem
+      fade
+      duration={0.4}
+      to="/projects"
+      show={props.show.toString()}
+      onClick={props.itemClicked}
+    >
+      Projects
+    </SiteNavItem>
+    <SiteNavItem
+      href="https://blog.scottosmith.net"
+      target="_blank"
+      rel="noreferrer"
+      show={props.show.toString()}
+      onClick={props.itemClicked}
+    >
+      Blog
+    </SiteNavItem>
+    <SiteNavItem
+      href="https://docs.google.com/document/d/1Fh2PoAmJKpbjjDKkW6PcoWVB-9NYbhKFDmPDztnm8LE/edit?usp=sharing"
+      target="_blank"
+      rel="noreferrer"
+      show={props.show.toString()}
+      onClick={props.itemClicked}
+    >
+      Resume
+    </SiteNavItem>
   </SiteNavContainer>
 );
 
 SiteNav.propTypes = {
-  show: PropTypes.string
-}
+  show: PropTypes.string,
+};
 
 SiteNav.defaultProps = {
-  show: 'false'
-}
+  show: 'false',
+};
 
 export default SiteNav;
