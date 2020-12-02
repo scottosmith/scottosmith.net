a75dfcd7045aab81e4d4b24b067d6f927871828a
Scott Smith <scott@scottosmith.net>
2020-12-02 14:14:40 -0600

    Change from docs to drive link

--- src/components/ui/site-nav/site-nav.js
+++ src/components/ui/site-nav/site-nav.js
@@ -34,7 +34,7 @@ const SiteNav = props => (
       Blog
     </SiteNavItem>
     <SiteNavItem
-      href="https://docs.google.com/document/d/1hPgnTDeLmWb7YXtHA4vODtG2EmZg0V7dv3uGYOsBd6M/edit?usp=sharing"
+      href="https://drive.google.com/file/d/1hPgnTDeLmWb7YXtHA4vODtG2EmZg0V7dv3uGYOsBd6M/view"
       target="_blank"
       rel="noreferrer"
       show={props.show.toString()}
